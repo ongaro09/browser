a7933f8f86914b8cd9a5c0fbcce2ab5a4a621fd8
Ongaro09 <ongaromark7@gmail.com>
2024-11-24 14:47:45 +0300

    solutions to tasks

---
+++ chapter-one_document/04-searching/00-script.js
@@ -0,0 +1,8 @@
+/* Solutions to tasks found in the exercises.md file */
+
+console.log(document.getElementById("age-table"));
+console.log(document.getElementsByName("age"));
+console.log(document.getElementById("age-table").children[0].children[0]);
+console.log(document.getElementsByName("search")[0]);
+console.log(document.getElementsByName("search")[0][0]);
+console.log(document.getElementsByName("search")[0][1]);
